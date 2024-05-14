7dc8bd20d9813500a658635f7190b6c6d56f3864
Rahul Kanwar <mailrahulkawnar@gmail.com>
2024-05-14 18:57:59 -0400

    checking

---
+++ Day3 - Treasure Island GAME/treasure_island.py
@@ -0,0 +1,7 @@
+print("Welcome to my Band Name Generator!! ")
+
+a = input("What is the name of the city you grew up in? \n")
+
+b = input("What is the name of your childhood pet? \n")
+
+print("You band name can be " + a  + ' ' + b)
