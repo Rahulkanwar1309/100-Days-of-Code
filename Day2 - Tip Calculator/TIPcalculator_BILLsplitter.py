c65efcd4d9a6f224de72bb5728d497ec35774b46
Rahulkanwar1309 <mailrahulkanwar@gmail.com>
2024-05-15 20:50:29 -0400

    improved

--- Day2 - Tip Calculator/TIPcalculator_BILLsplitter.py
+++ Day2 - Tip Calculator/TIPcalculator_BILLsplitter.py
@@ -1,8 +1,10 @@
 print("Welcome to tip Calculator! ")
 bill = float(input("What was the total bill? \n"))
-tip = float(input("How much tip will you like to give? (in %) \n"))
+tip_percent = float(input("How much tip will you like to give? (in %) \n"))
 
-total_bill = bill + ((tip/100)*bill)
+tip = (tip_percent/100)*bill
+
+total_bill = bill + tip
 
 print("Your total bill after tip is ", total_bill)
 
@@ -12,7 +14,7 @@ split = input("Do you wan to split the bill? (Y or N) \n")
 if split == 'Y':
     number_of_people = int(input("How many people to split the bill? \n"))
     per_person = total_bill/number_of_people
-    print("Each person should pay: ", per_person)
+    print("Each person should pay: $", "{:.2f}".format(per_person))
 elif split == 'N':
     print("Have a Good Day!!")
 else:
