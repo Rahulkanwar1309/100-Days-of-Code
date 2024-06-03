f4c8c17698c38ef312df1afc501b1e021ebc9fc9
Rahulkanwar1309 <mailrahulkanwar@gmail.com>
2024-06-03 13:55:57 -0400

    today's work

--- Day15 - /coffee.py
+++ Day15 - /coffee.py
@@ -2,3 +2,17 @@ from data import MENU, resources
 
 user = input("What Would You Like To Drink ? (espresso/latte/cappuccino) : ")
 
+def report():
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+    money = resources["money"]
+    print(f"""
+    Water : {water}
+    Milk : {milk}
+    Coffee : {coffee}
+    Money : {money}
+    """)
+
+def resource_check():
+    
