2bad8c11915bf78cf456a7390f98ff77f50dc17a
Rahulkanwar1309 <mailrahulkanwar@gmail.com>
2024-06-11 16:15:51 -0400

    ..

---
+++ Day18 - Turtle Graphics/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+tom = Turtle()
+
+
+tom.forward(100)
+tom.left(90)
+tom.forward(100)
+tom.left(90)
+tom.forward(100)
+tom.left(90)
+tom.forward(100)
+
+s= Screen()
+
+s.exitonclick()
+
+import heroes
+
+print(heroes.gen())
